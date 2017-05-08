1146b45b13a4819d4e2e9ec35d3e855905073d3a
João Vicente Souto <joaovicentesouto@gmail.com>
2017-05-08 15:29:15 -0300

    Resolução final - lab 5

--- lab_5/exercicio.asm
+++ lab_5/exercicio.asm
@@ -1,60 +1,81 @@
 .data
-_v: .word ....
-_n: .word ....
+_v: .word 29,28,27,26,25,24,23,22,21,-1
+_n: .word 10
 
 .text
 .globl main
 
 main:
-  la      $a0,_v
-  lw      $a1,_n
-  jal     sort
-  li      $v0,10      #Exit syscall
+  la	$a0, _v
+  lw $a1, _n
+  jal	sort
+  li	$v0, 10	#Exit syscall
   syscall
 
-# procedure sort
+# procedure bubble-sort
 sort:
-  # preserva��o de registradores armazenando seus conte�dos na pilha
-  addi    $sp,  $sp,    -12
-  sw      $ra,  8($sp)
-  sw      $s1,  4($sp)
-  sw      $s0,  0($sp)
+    # salvamento do contexto
+    addi	$sp, $sp, -20
+    sw	$ra, 8($sp)
+    sw	$s1, 4($sp)
+    sw	$s0, 0($sp)
+    sw	$s2, 12($sp)
+    sw	$s3, 16($fp)
 
-  move    $s0,  $zero   # inicializa��o de i
-  # in�cio do corpo do la�o externo
-for1tst:
-  nop #MARCA 1
-  slt     $t0,  $s0,    $a1 # for1st
-  beq     $t0,  $zero,  exit1
-  addi    $s1,  $s0,    -1
-  # inicio do corpo do la�o interno
-for2tst:
-  slti    $t0,  $s1,    0  # for2st
-  bne     $t0,  $zero,  exit2
-  sll     $t1,  $s1,    2
-  add     $t2,  $a0,    $t1
-  lw      $t3,  0($t2)
-  lw      $t4,  4($t2)
-  slt     $t0,  $t4,    $t3
-  beq     $t0,  $zero,  exit2
+    move $s2, $a0	# s2 = V[]
+    move $s3, $a1	# s3 = n
+    move	$s0, $zero	# i = 0
 
-  nop # MARCA 2
-  jal     swap 	# chamada de swap
-  addi    $s1,  $s1,    -1
-  j       for2tst
-  # fim do corpo do la�o interno
-exit2:
-  addi    $s0,  $s0,    1
-  j       for1tst
-  # fim do corpo do la�o externo
-exit1:
-  # restaura��o de conte�dos de registradores preservados na pilha
-  lw      $s0,  0($sp)
-  lw      $s1,  4($sp)
-  lw      $ra,  8($sp)
-  addi    $sp,  $sp,    12
-  # retorno ao procedimento chamador
-  jr      $ra
+  # primeiro loop
+  for1tst:
+    nop #MARCA 1
+    slt	$t0, $s0, $s3	# i < n
+    beq	$t0, $zero, exit1
+    addi	$s1, $s0, -1	# j = i-1
 
- # codifica��o da procedure swap
+  # segundo looṕ
+  for2tst:
+    slti	$t0, $s1, 0	# j < 0
+    bne	$t0, $zero, exit2
+    sll	$t1, $s1, 2	# j * 4
+    add	$t2, $s2, $t1	# j + V[]
+    lw	$t3, 0($t2)	# V[j]
+    lw	$t4, 4($t2)	# V[j+1]
+    slt	$t0, $t4, $t3	# V[j+1] < V[j]
+    beq	$t0, $zero, exit2
+
+    move	$a1, $s1	# a1 = j
+    move	$a0, $s2
+    nop # MARCA 2
+    jal	swap # swap
+    addi	$s1, $s1, -1	# j--
+    j	for2tst
+
+  # vai embora
+  exit2:
+    addi	$s0, $s0, 1	# i++
+    j	for1tst	# volta pro loop do i
+
+  exit1:
+    # restauração de contexto
+    lw	$s0, 0($sp)
+    lw	$s1, 4($sp)
+    lw	$ra, 8($sp)
+    lw	$s2, 12($sp)
+    lw	$s3, 16($sp)
+    addi	$sp, $sp, 20
+    # returns
+    jr	$ra
+
+# swap
 swap:
+  # calculo de indices
+  sll	$t0, $a1, 2	# k * 4
+  add	$a0, $t0, $a0	# k + V[]
+  # realiza a troca
+  lw	$t1, 0($a0)
+  lw	$t2, 4($a0)
+  sw	$t2, 0($a0)
+  sw	$t1, 4($a0)
+  # retorno ao programa principal
+  jr	$ra
